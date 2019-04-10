eb44b08b2060e9bd48046e1cf8ea7c39b8816139
Ashley Miller <asm2fe@virginia.edu>
2019-04-10 01:12:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,13 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
+  @@genres = []
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count +=1
-    @@artists << artist
+    @@artists << artist if !@@artists.include?(artist)
+    @@genres << genre if !@@genres.include?(genre)
   end
 end
